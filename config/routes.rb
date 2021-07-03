21ec14ffcc3a69d860b1835bbef1d1b4c8085fae
Kliti <68862849+KlitiHamataj@users.noreply.github.com>
2021-07-03 19:53:50 +0200

    Revert "Revert "Adding short links""

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :articles, except: :destroy
   root to: 'articles#index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :articles, except: :destroy
+  get '/:id' => "shortener/shortened_urls#show"
 end
