174fe1b44ce0291e06a153fed2b1a57135c19de8
Kliti <hamatajkliti@gmail.com>
2021-07-09 16:09:43 +0200

    url shortener feature done

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :articles, except: :destroy
+  get '/:id' => "shortener/shortened_urls#show"
   root to: 'articles#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
